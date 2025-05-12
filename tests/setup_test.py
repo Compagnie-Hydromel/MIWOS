b770331b3cd6822b1a0f98aad2673b588d06c8ea
Ethann <ethann@shkermit.ch>
2025-05-12 09:44:26 +0200

    Remove assertion for unrecognized option in setup test

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -6,5 +6,3 @@ class TestSetup(unittest.TestCase):
     def test_main(self):
         with self.assertRaises(SystemExit) as cm:
             import setup
-        self.assertIn("error: option --udiscovery not recognized",
-                      str(cm.exception))
